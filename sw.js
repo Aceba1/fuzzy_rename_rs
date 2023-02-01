42a3a28913b9e794ce5db84d57b467c4d2640053
Aceba1 <aceba1@outlook.com>
2023-02-01 01:01:27 +0000

    Deploying to gh-pages from @ Aceba1/fuzzy_rename_rs@1d97d625474f839c1d0a1e3c3d99c61be17ebfee 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'fuzzy-rename-rs-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './fuzzy_rename_rs.js',
+  './fuzzy_rename_rs_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
